e27b6eccf45f0e5e9a33d2463a502c41c74cbca6
Pronepoet <ochiengphillip4@gmail.com>
2022-08-27 02:44:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name,email){
+ 
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({name,email}),
+      };
+      return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        const cre = document.createElement('p')
+        cre.textContent = data.id
+        console.log(data.id)
+        const bdy = document.querySelector('body')
+        bdy.appendChild(cre)
+       
+      })
+      .catch((error)=> {
+      document.querySelector('body').textContent = `${error.message}`
+      });
+      
+}
+submitData('steve','steve@steve.com')
